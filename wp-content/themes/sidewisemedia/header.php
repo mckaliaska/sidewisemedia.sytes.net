3a6ee1f81089bf4432b9941ac18758f333defdac
ginlin <montazhnik3@gmail.com>
2013-09-03 23:02:28 +0300

    33

--- wp-content/themes/sidewisemedia/header.php
+++ wp-content/themes/sidewisemedia/header.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8" />
 	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+	<script src="js/default.js"></script>
 	<title></title>
 	<meta name="keywords" content="" />
 	<meta name="description" content="" />
