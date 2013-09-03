eea0cebe981d923926a3f22aa7cd28780339d7cf
Tolian <demchenko.anatolioy@yandex.ru>
2013-09-03 22:32:00 +0300

    push#1

---
+++ wp-content/themes/sidewisemedia/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8" />
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+	<title></title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<!--WP-generated header -->
+</head>
+<body>
+header
